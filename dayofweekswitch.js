4c8924994c2729a89af7b8faf54d7770b07a775a
Vlad Trebukhov <33733297+vladtrebukhov@users.noreply.github.com>
2018-03-15 18:35:26 -0400

    Add files via upload
    message change depending on day of week switch practice

---
+++ dayofweekswitch.js
@@ -0,0 +1,34 @@
+const day = new.Date().getDay();
+
+switch (day) {
+  case 1:
+    console.log("Shit its Monday");
+    break;
+  case 2:
+    console.log("It's only Tuesday");
+    break;
+  case 3:
+    console.log("Happy Hump Day")
+    break;
+  case 4:
+    console.log("Thirsty Thursdays")
+    break;
+  case 5:
+    console.log("TGIF")
+    break;
+  case 6:
+    console.log("It's finally Saturday")
+    break;
+  case 7:
+    console.log("Back to work tomorrow soz")
+    break;
+  default:
+    console.log("Something didn't work...")
+    break;
+
+
+
+
+
+
+}
